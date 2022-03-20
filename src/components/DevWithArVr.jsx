d7d243f370498c365143e5707f5d926963ba2553
Liza <liziaka@mail.ru>
2022-03-20 21:06:30 +0300

    background images for "about us" page

--- src/components/DevWithArVr.jsx
+++ src/components/DevWithArVr.jsx
@@ -1,8 +1,8 @@
 import '../css/DevWithArVr.css';
 import arrow from "../img/background/Vector 85.svg";
-import videoArVr from "../img/background/Video.png";
-import light1 from "../img/background/Ellipse 58.svg";
-import light2 from "../img/background/Ellipse 57.svg";
+
+// import light1 from "../img/background/Ellipse 58.svg";
+// import light2 from "../img/background/Ellipse 57.svg";
 
 
 function DevWithArVr() {
@@ -22,12 +22,7 @@ function DevWithArVr() {
 					<p className="purple-text basic-text">Наши проекты</p>
 					<img src={arrow} alt="arrow" className="arrow"/>
 				</div>
-			{/*<video autoplay loop class="video-background-AR-VR">
-       			<source src={videoArVr} type="video/mp4"/>
-    		</video>*/}	
-    		<img src={videoArVr} alt="VideoARVR" className="video-background-AR-VR" />
-    		<img src={light1} alt="background light" className="background-lights light1-AR-VR"/>
-    		<img src={light2} alt="background light" className="background-lights light2-AR-VR"/>			
+			
 			</div>
 		</div>
 		);
